afc7e0b7020601c2f04feb571977579eec91abb8
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-06-20 01:23:12 +0200

    changes

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import {ContactForm} from "../../components/ContactForm/ContactForm";
+import ContactForm from "../../components/ContactForm/ContactForm";
 import {SearchBox} from "../../components/SearchBox/SearchBox";
 import {ContactList} from "../../components/ContactList/ContactList"; 
 
 import { fetchContacts } from "../../redux/contacts/operations";
-import { selectLoading } from "../../redux/contacts/selectors";
+import { selectLoadingContacts } from "../../redux/contacts/selectors";
 
 export default function ContactsPage() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectLoading);
+  const isLoading = useSelector(selectLoadingContacts);
 
   useEffect(() => {
     dispatch(fetchContacts());
