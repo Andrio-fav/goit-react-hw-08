ae18161bc48c2c740bdd47f2d7a701f1c0d5273f
Andrio-fav <andriy2006lukashuk@gmail.com>
2025-06-21 17:46:04 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from '../redux/auth/selectors'; // адаптуй шлях за потреби
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
 export default function HomePage() {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
     <div>
-      <h1>Contacts manager welcome page</h1>
+      <h1>Contacts Manager</h1>
       {!isLoggedIn ? (
         <p>
           Щоб розпочати, будь ласка,&nbsp;
@@ -16,7 +16,7 @@ export default function HomePage() {
         </p>
       ) : (
         <p>
-          Ви вже авторизовані — перейдіть до <Link to="/contacts">контактів</Link>.
+          Ви вже увійшли. Перейдіть до <Link to="/contacts">контактів</Link>.
         </p>
       )}
     </div>
